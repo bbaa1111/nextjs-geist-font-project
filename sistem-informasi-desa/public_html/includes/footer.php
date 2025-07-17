2da73834018459155400af9d5ca329ec56aa4d9c
bbaa1111 <bba.aku2023@gmail.com>
2025-07-18 01:04:18 +0700

    Add sistem-informasi-desa/public_html/includes/footer.php 1752775457308

---
+++ sistem-informasi-desa/public_html/includes/footer.php
@@ -0,0 +1,18 @@
+</main>
+
+    <footer class="bg-light mt-5 py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p class="mb-0">&copy; <?php echo date('Y'); ?> <?php echo SITE_NAME; ?>. All rights reserved.</p>
+                </div>
+                <div class="col-md-6 text-md-end">
+                    <p class="mb-0">Dibuat dengan PHP Native & MySQL</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
